88f05c5dea1f2897e4ed8807f864facd314a473a
SomeBlu <webmaster@someblu.com>
2019-12-28 01:42:06 +0800

    validate rpc server to ensure it is working

--- update_trackers.zsh
+++ update_trackers.zsh
@@ -22,6 +22,25 @@ write_to_log "new trackers: ${trackers}"
 uuid=${$(uuidgen 2>/dev/null):-$(od -x /dev/urandom | head -1 | awk '{OFS="-"; print $2$3,$4,$5,$6,$7$8$9}' | tr '[a-z]' '[A-Z]')}
 rpc_secret=$(perl -ne 'print if s/^rpc-secret=(.+$)/${1}/' "${confPath}")
 rpc_listen_port=${$(perl -ne 'print if s/^rpc-listen-port=([0-9]+$)/${1}/' "${confPath}"):-6800}
+
+send_to_server() {
+  curl -H "Accept: application/json" -H "Content-type: application/json" -X "POST" -d "$json" -m 5 -s "localhost:${rpc_listen_port}/jsonrpc"
+}
+
+write_to_log "validate rpc server: localhost:${rpc_listen_port}"
+json='{
+  "jsonrpc": "2.0",
+  "method": "aria2.getVersion",
+  "id": "'${uuid}'",
+  "params": [
+    "token:'${rpc_secret}'"
+  ]
+}'
+until send_to_server &>/dev/null; do
+    write_to_log "wait for server response"
+    sleep 5
+done
+
 json='{
   "jsonrpc": "2.0",
   "method": "aria2.changeGlobalOption",
@@ -35,4 +54,4 @@ json='{
 }'
 write_to_log "prepare to send json:\n${json}"
 
-write_to_log "localhost:${rpc_listen_port} respond with $(curl -H "Accept: application/json" -H "Content-type: application/json" -X "POST" -d "${json}" -s "localhost:${rpc_listen_port}/jsonrpc")"
+write_to_log "localhost:${rpc_listen_port} respond with $(send_to_server)"
